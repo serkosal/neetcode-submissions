891445daaf6bfd03e7d0851334c899e95018e4aa
Sergey Kosykh <s3rkosal@gmail.com>
2026-06-17 00:01:08 +0400

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int, int> hash;
+
+        for (int num : nums) 
+            if (++hash[num] > 1) return true;
+
+        return false;
+    }
+};
