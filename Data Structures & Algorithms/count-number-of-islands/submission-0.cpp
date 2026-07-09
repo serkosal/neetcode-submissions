c1726a6b6be42c928a185d0abc591fe01d9e49b6
Sergey Kosykh <s3rkosal@gmail.com>
2026-07-09 11:36:55 +0400

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int res = 0;
+
+        const int ROWS = grid.size(), COLS = grid[0].size();
+        set<pair<int, int>> visited;
+        for (int r = 0; r < ROWS; ++r) {
+            for (int c = 0; c < COLS; ++c) {
+                if (grid[r][c] == '0' || visited.contains({r, c})) continue;
+                ++res;
+
+                for (
+                    auto to_visit = queue{deque{pair<int, int>{r, c}}};
+                    to_visit.size();
+                ) {
+                    auto [y, x] = to_visit.front(); to_visit.pop();
+
+                    if (visited.contains({y, x})) continue;
+                    if (x < 0 || x >= COLS || y < 0 || y >= ROWS) continue;
+                    if (grid[y][x] == '0') continue;
+
+                    visited.insert({y, x});
+                    to_visit.push({y + 1, x});
+                    to_visit.push({y - 1, x});
+                    to_visit.push({y, x + 1});
+                    to_visit.push({y, x - 1});
+                }
+            }
+        }
+
+        return res;
+    }
+};
