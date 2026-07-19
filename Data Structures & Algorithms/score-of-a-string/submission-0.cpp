09181a21d1de5181f2ac32fb13a86a2a0431fa91
Sergey Kosykh <s3rkosal@gmail.com>
2026-07-19 17:31:26 +0400

    Add: score-of-a-string - submission-0

---
+++ Data Structures & Algorithms/score-of-a-string/submission-0.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int scoreOfString(const string& s) {
+        int res = 0;
+        for (int i = 1; i < s.size(); ++i)
+            res += abs(s[i] - s[i - 1]);
+        return res;
+    }
+};
